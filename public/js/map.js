ff310c06cccf6daca69b0615e5088fe535c4e1a6
smsmmessi <hossam.m.cv@gmail.com>
2017-12-13 14:16:48 +0200

    custome map marker

--- public/js/map.js
+++ public/js/map.js
@@ -1,17 +1,19 @@
 function initMap() {
-    var uluru = {lat: 23.885942, lng: 45.079162};
+    var uluru = new google.maps.LatLng(23.885942, 45.079162);
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 6,
       center: uluru,
-      disableDefaultUI: true
     });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-      zoom: 15
-    });
-    google.maps.event.addListener(marker, 'click', function() {
-        map.panTo(this.getPosition());
-        map.setZoom(15);
-        });  
+    // var marker = new google.maps.Marker({
+    //   position: uluru,
+    //   map: map,
+    //   zoom: 15
+    // });
+    var overlay = new CustomMarker(uluru, map, {"id": 30, "name": "الرياض"});
+    // google.maps.event.addListener(marker, 'click', function() {
+    //     map.panTo(this.getPosition());
+    //     map.setZoom(15);
+    //     });  
   }
+  google.maps.event.addDomListener(window, 'load', initMap);
+  
--- resources/views/components/mapSet.blade.php
+++ resources/views/components/mapSet.blade.php
@@ -4,8 +4,10 @@
 
 
 @push('scripts')
-    <script src="{{ asset('js/map.js') }}"></script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCuaq7NJkSDoz9ORGZzVopdHK6X-m8F6qs&callback=initMap">
+<script type="text/javascript"
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCuaq7NJkSDoz9ORGZzVopdHK6X-m8F6qs">
     </script>
+    <script type="text/javascript" src="{{ asset('js/customMarker.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/map.js') }}"></script>
+   
 @endpush

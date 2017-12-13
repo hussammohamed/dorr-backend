2db0d09b0609f3119a38a4e3e8c3fe915f0d2999
smsmmessi <hossam.m.cv@gmail.com>
2017-12-13 12:20:34 +0200

    create map c, v-card-ad

---
+++ resources/views/components/appsDownload.blade.php
@@ -0,0 +1,8 @@
+<div class="apps--card">
+    <div class="apps--card__bg"></div>
+    <div class="apps--card__mobile"></div>
+    <div class="apps--card__header">
+            <h3 class="apps--card__title">تطبيق دور</h3>
+            <img class="apps--card__logo" src={{ asset('images/dorr-logo.svg') }} alt="">
+    </div>
+</div>
---
+++ resources/views/components/vCard.blade.php
@@ -0,0 +1,19 @@
+<div class="mdl-card mdl-shadow--2dp v-card">
+  <div class="card--item card--item__img">
+    <img src={{ asset('images/card1.png') }} alt="">
+  </div>
+  <div class="card--item card--item__text">
+    <h5 class="card--text__title">فيلا للبيع في حي الراشدية</h5>
+    <p class="card--text__address">فيلا للبيع في حي الراشدية</p>
+    <span class="card--text__size"> 405 م<sup>2</sup></span>
+  </div>
+
+    <div class="card-footer">
+        <div class="card-footer__price">
+            <span class="price--text">
+                290,000 ريال
+            </span>
+        </div>
+
+    </div>
+</div>

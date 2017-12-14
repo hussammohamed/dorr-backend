1dcb2618f4dced5446269766156f880ccea4e67a
smsmmessi <hossam.m.cv@gmail.com>
2017-12-14 15:30:58 +0200

    custom marker

--- public/js/customMarker.js
+++ public/js/customMarker.js
@@ -11,30 +11,37 @@ CustomMarker.prototype.draw = function() {
 	var self = this;
 	
 	var div = this.div;
-	
+	var span = this.span;
 	if (!div) {
 	
 		div = this.div = document.createElement('div');
 		
-		div.className = 'mdl-button mdl-js-button mdl-button--raised mdl-button--colored hossam';
+		div.className = 'mdl-button mdl-js-button mdl-button--raised mdl-button--colored map-marker';
 		div.style.position = 'absolute';
+		span = this.span = document.createElement('span');
+		span.className = "map-marker__arrow"
 		if (typeof(self.args.name) !== 'undefined') {
 			div.innerHTML += self.args.name;
 		}
 		
 		google.maps.event.addDomListener(div, "click", function(event) {			
-			console.log("hossam")
+			//console.log("hossam")
 		});
 		
 		var panes = this.getPanes();
 		panes.overlayImage.appendChild(div);
+		panes.overlayImage.appendChild(span);
+
+		
 	}
 	
 	var point = this.getProjection().fromLatLngToDivPixel(this.latlng);
 	
 	if (point) {
-		div.style.left = point.x + 'px';
-		div.style.top = point.y + 'px';
+		div.style.left = (point.x - 32) + 'px';
+		div.style.top = (point.y - 32) + 'px';
+		span.style.left = (point.x - 6) + 'px'
+		span.style.top =  (point.y - 8) + 'px';               
 	}
 	if (typeof(self.args.id) !== 'undefined') {
 		div.dataset.id = self.args.id;
--- public/js/map.js
+++ public/js/map.js
@@ -1,19 +1,40 @@
+var regions = [
+    {
+        "id": 1,
+        "title": "الرياض",
+        "location": {
+            "lat": "24.713481",
+            "long": "46.6752"
+        }
+    },
+    {
+      "id": 2,
+      "title": "المدينة",
+      "location": {
+          "lat": "24.523575",
+          "long": "39.568892"
+      }
+  },
+  {
+    "id": 3,
+    "title": "مكة",
+    "location": {
+        "lat": "21.388093",
+        "long": "39.858227"
+    }
+},
+]
+
 function initMap() {
-    var uluru = new google.maps.LatLng(23.885942, 45.079162);
+    var uluru = new google.maps.LatLng(23.128363, 37.199707);
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 6,
       center: uluru,
+      disableDefaultUI: true
     });
-    // var marker = new google.maps.Marker({
-    //   position: uluru,
-    //   map: map,
-    //   zoom: 15
-    // });
-    var overlay = new CustomMarker(uluru, map, {"id": 30, "name": "الرياض"});
-    // google.maps.event.addListener(marker, 'click', function() {
-    //     map.panTo(this.getPosition());
-    //     map.setZoom(15);
-    //     });  
+    regions.forEach(function(el) {
+      var overlay = new CustomMarker(new google.maps.LatLng(el.location.lat, el.location.long), map, {"id": el.id, "name": el.title});
+  });  
   }
   google.maps.event.addDomListener(window, 'load', initMap);
   

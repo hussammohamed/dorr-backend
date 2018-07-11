2b57b4c74dabb35305fc72cd31c1f44e09ffaf1b
hossam <rashwan@hossams-MacBook-Pro.local>
2018-07-11 14:19:56 +0200

    fix issues in user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,7 +26,7 @@ class UserController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth');//->except('index','show');
+        $this->middleware('auth:api');//->except('index','show');
     }
 
     /**

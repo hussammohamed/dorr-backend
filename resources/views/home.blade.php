f54c63bdc3f25d2961f238ec75646ca5504e7ef9
Mohamed Mohiy <mohamedmohiy@gmail.com>
2018-07-20 17:06:47 +0200

    firebase js code update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -36,28 +36,28 @@
 </div>
 
 @endsection @push('scripts')
-
-<script src="https://www.gstatic.com/firebasejs/5.3.0/firebase.js"></script>
-<script>
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDvN0Pywabdl8U8J2hxM0b32h7ErOZuXZ8",
-    authDomain: "dorr-app.firebaseapp.com",
-    databaseURL: "https://dorr-app.firebaseio.com",
-    projectId: "dorr-app",
-    storageBucket: "dorr-app.appspot.com",
-    messagingSenderId: "243311451130"
-  };
-  firebase.initializeApp(config);
-</script>
-
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js"></script>
 <script type="text/javascript" src={{ asset( 'js/owl.carousel.rtl.js')}} /></script>
-<script>
-$(".filter-map").owlCarouselRtl({
-        dotsData: false,
-        nav: true,
-        items: 10
+<script>$(".owl-carousel").owlCarouselRtl({
+        margin: 25,
+        loop: true,
+        autoWidth: true,
+        items: 4,
+        center: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 3,
+                nav: false
+            },
+            1024: {
+                items: 3,
+
+            }
+        }
     });
 </script> @endpush @push('styles')
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css">

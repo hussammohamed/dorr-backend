a0e11efa74665c6432f486c55cfd35ed33b7f6bc
Mohamed Mohiy <mohamedmohiy@gmail.com>
2018-07-30 04:22:30 +0200

    firebase test

--- app/Http/Controllers/ContractController.php
+++ app/Http/Controllers/ContractController.php
@@ -53,10 +53,42 @@ class ContractController extends Controller
         return [ $this->modelnames => ContractResource::collection(Contract::Where('m_property_id',$id)->get())];
     }
 
+
+
+    
+    public function sendFCM($mess,$id) {
+        $url = 'https://fcm.googleapis.com/fcm/send';
+        $fields = array (
+                'to' => $id,
+                'notification' => array (
+                        "body" => $mess,
+                        "title" => "Title",
+                        "icon" => "myicon"
+                )
+        );
+        $fields = json_encode ( $fields );
+        $headers = array (
+                'Authorization: key=' . "AIzaSyDvN0Pywabdl8U8J2hxM0b32h7ErOZuXZ8",
+                'Content-Type: application/json'
+        );
+        
+        $ch = curl_init ();
+        curl_setopt ( $ch, CURLOPT_URL, $url );
+        curl_setopt ( $ch, CURLOPT_POST, true );
+        curl_setopt ( $ch, CURLOPT_HTTPHEADER, $headers );
+        curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt ( $ch, CURLOPT_POSTFIELDS, $fields );
+        
+        $result = curl_exec ( $ch );
+        curl_close ( $ch );
+    }
+
+
     public function notify()
     {
         $user = User::find(Auth::user()->id);
         $user->notify(new AppNotification);
+        $this->sendFCM("msg","1");
         return "Notifiction had been sent";
 
         

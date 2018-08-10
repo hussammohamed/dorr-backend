2988ca33c142633e475a954ae1871545f473ec93
hossam <rashwan@hossams-MacBook-Pro.local>
2018-08-10 17:46:07 +0200

    fix serach slow

--- resources/views/searchPage.blade.php
+++ resources/views/searchPage.blade.php
@@ -11,3 +11,16 @@
     </script>
         <script type="text/javascript" src="{{ asset('js/customMarker.js') }}"></script>
     @endpush
+    <script src="/js/echo.min.js"></script>
+    <script>
+  echo.init({
+    offset: 100,
+    throttle: 250,
+    unload: false,
+    callback: function (element, op) {
+      console.log(element, 'has been', op + 'ed')
+    }
+  });
+
+  // echo.render(); is also available for non-scroll callbacks
+  </script>

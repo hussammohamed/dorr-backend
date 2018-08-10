cd992757911128cb4ee8d75107db73dfd45413f2
hossam <rashwan@hossams-MacBook-Pro.local>
2018-08-10 17:16:18 +0200

    fix serach slow

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -75,3 +75,12 @@ function loginShow(currentUrl){
       document.getElementById('filterItems').attachEvent("onmousewheel", scrollHorizontally);
   }
 })();
+
+window.addEventListener('load', function(){
+  var allimages= document.getElementsByTagName('img');
+  for (var i=0; i<allimages.length; i++) {
+      if (allimages[i].getAttribute('data-src')) {
+          allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
+      }
+  }
+}, false)
